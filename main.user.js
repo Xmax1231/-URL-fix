cd758c11ee49519fbdec281141f7fdeb363c34ff
Xmax1231 <xmax861231@gmail.com>
2017-04-10 16:50:29 +0800

    first commit

---
+++ main.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         巴哈姆特電玩資訊站 URL fixed
+// @namespace    https://www.facebook.com/xmax1231
+// @version      1.0
+// @description  gamer.com.tw url fixed
+// @author       XMAX
+// @match        https://m.gamer.com.tw/*
+// @grant        none
+// ==/UserScript==
+
+(function(){
+    if(location.href.search(/m.gamer.com.tw\/forum/)!=-1){
+        location.href=location.href.replace(/m.gamer.com.tw\/forum/i,"forum.gamer.com.tw");
+    }
+})();
